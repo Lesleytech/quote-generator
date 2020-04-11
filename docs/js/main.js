b710def7fdd5f30603200eb31460c46f12e7cb0e
Lafen Lesley <lesleytech6@gmail.com>
2020-04-11 23:50:17 +0300

    Initial commit

---
+++ docs/js/main.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    getQuote();
+}
+
+const loadingContainer = document.getElementById("loading-div"),
+    quotesContainer = document.getElementById("quotes-container"),
+    shuffleButton = document.getElementById("shuffle"),
+    quoteInfo = document.getElementById('quote-info');
+
+function getQuote() {
+    fetch("https://type.fit/api/quotes")
+        .then(response => response.json())
+        .then(data => {
+            loadingContainer.style.display = "none";
+            quotesContainer.style.display = "block";
+            shuffleButton.innerHTML = "Shuffle";
+            shuffleButton.addEventListener("click", () => {
+                showQuotes(data);
+            });
+            showQuotes(data);
+        })
+        .catch(() => {
+            loadingContainer.style.display = "none";
+            quotesContainer.style.display = "block";
+            quoteInfo.innerHTML = `<div id="error">Failed to load quote!!!</div>`;
+            shuffleButton.innerHTML = "Reload"
+            shuffleButton.addEventListener("click", () => {
+                getQuote();
+            });
+        });
+}
+
+function showQuotes(quotes) {
+    const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
+    quoteInfo.style.opacity = 0;
+    setTimeout(() => {
+        quoteInfo.style.opacity = 100;
+        quoteInfo.innerHTML = `
+            <div id="quote">
+                <i class="fa fa-quote-left" aria-hidden="true"></i>
+                ${randomQuote.text}
+            </div>
+            <div id="author">${randomQuote.author === null ? "Anonymous" : randomQuote.author}</div> `;
+    }, 500)
+}
